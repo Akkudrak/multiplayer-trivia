24b00ed49344ba89e02d4941c561225e527795d2
Akkudrak <akkudrak@gmail.com>
2024-12-09 18:59:20 -0600

    Formacion de Equipos, Lista final o casi de los participantes.

--- scenes/sorting_scene.js
+++ scenes/sorting_scene.js
@@ -1,49 +1,67 @@
+var ticks=0;
+var indexBoard=0;
+var boards=[];
 export class sorting_scene extends Phaser.Scene{
     constructor () {
         super({ key: 'sorting_scene' });
     }
 
+    preload(){
+        for (var i = 0; i < 10; i++) {
+            this.textures.addImage('team_'+(i+1)+'_img', document.getElementById('team_'+(i+1)+'_img'));
+        }
+        
+        
+    }
+
     create (){   
         this.add.image(0, 0, 'bg_main').setDisplaySize(w_width,w_height).setOrigin(0,0);
-        this.drawTeams();
+        this.add.image(mid_h, (mid_v/10), 'title_equipos').setOrigin(.5,0).setScale(1.5);
 
+        var col=209;
+        for (var i = 0; i < 10; i++) {
+            boards.push(this.add.image(col, mid_v, 'team_'+(i+1)+'_img').setDepth(1));
+            col+=167;
+        }
 
+        var mainScene=this.scene.get('mainScene');
+        mainScene.setButton({scene:this, x:mid_h,y:w_height-(mid_mid_v/4),text:"Siguiente",scale_c:scale}).setInteractive().on("pointerdown", () => {
+            this.scene.start("presentation_scene");
+        });
+
+        this.tweens.add({
+            targets: boards[indexBoard],
+            depth: 2, // Posición final en el eje Y (centro de la escena)
+            scale:1.1,
+            ease: 'Power2', // Tipo de interpolación
+            duration: 500, // Duración en milisegundos
+        });
+        indexBoard++;
 
     }
 
-    drawTeams(){
-        var cols_t=8;
-        var row_ini=mid_v;
-        var scaleHere=.4
-        let col_size_t=(w_width-(padding*base_cols))/base_cols;
-        let cols_t_a=[];
-        let base_sum_t=10;
-        var index_c=0;
-
-        for (var i = 0; i < cols_t; i++) {
-            base_sum_t+=(padding+10)+(col_size_t)
-            cols_t_a.push(base_sum_t);
-        }
-
-        console.log(cols_t_a);
-        
-
-        for (var i = 0; i < users.length; i++) {
-            console.log(users[i].nombre);
-            this.drawUser({scene:this,x:cols_t_a[index_c],y:row_ini,scale_c:scaleHere,text:users[i].nombre});
-            index_c++;
-            if (index_c>=cols_t) {
-                index_c=0;
-                row_ini+= 40;
+    update(){
+        ticks++;
+        if (ticks>=500) {
+            for (var i = 0; i < boards.length; i++) {
+                boards[i].setScale(1);
+                boards[i].setDepth(1);
             }
+            ticks=0;
+            this.tweens.add({
+                targets: boards[indexBoard],
+                depth: 2, // Posición final en el eje Y (centro de la escena)
+                scale:1.1,
+                ease: 'Power2', // Tipo de interpolación
+                duration: 500, // Duración en milisegundos
+            });
+            if (indexBoard>=boards.length-1) {
+                indexBoard=0;
+            }else{
+                indexBoard++;
+            }
+            
         }
-    }
-
-    drawUser(data=null){
-        //var button=data.scene.add.image(data.x, data.y, 'btn_lg_white').setOrigin(0,.5).setScale(.3,data.scale_c);
-        console.log(data.x);
-        data.scene.add.text(data.x, data.y, data.text, { fontFamily: 'mikado', fontSize: 15,color:"#000000" }).setOrigin(0, .5)//.setStroke('#2F4649',10);
-        
     }
 
 
